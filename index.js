77d95407c688698e4799bd13b0293292cd58b597
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2022-07-14 23:55:46 +0600

    github

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin:uk3AHHuREp8ijSBv@mycluster.rzcci.mongodb.net/fcc-url-shortener?retryWrites=true",{useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
   username: {
