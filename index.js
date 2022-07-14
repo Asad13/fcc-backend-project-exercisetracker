62f488ce5258f6a4eb8f8d3c2f7732b59e67c3e4
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2022-07-14 19:50:01 +0600

    Initial Commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
